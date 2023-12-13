3d50198da700391a2a6e630a413105a57c62f483
nikoac <136534522+nikoac@users.noreply.github.com>
2023-12-13 01:45:35 -0300

    agregando about us page

---
+++ src/pages/AboutUsPage/AboutUsPage.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import '../../styles/styles.css'
+
+
+const AboutUsPage = () => {
+  return (
+<div>    
+  <div className='container d-flex justify-content-center'><h1 className='text-center fw-bold'>Meet our Team</h1></div>  
+	<div id="slider">
+    <div id="slide-track">
+        <div id="slide">
+
+        </div>
+        <div id="slide">
+
+        </div>
+        <div id="slide">
+
+        </div>
+        <div id="slide">
+
+        </div>
+        <div id="slide">
+
+        </div>
+        <div id="slide">
+
+        </div>
+        <div id="slide">
+
+        </div>
+        <div id="slide">
+
+        </div>
+        <div id="slide">
+
+        </div>
+        <div id="slide">
+
+        </div>
+    </div>
+  </div>
+</div>
+  )
+}
+
+export default AboutUsPage
