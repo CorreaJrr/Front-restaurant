9136ef7e71da82c9e74eba47c33c7e2548f4adb5
nikoac <136534522+nikoac@users.noreply.github.com>
2023-12-14 15:25:36 -0300

    navbar +

--- src/components/navbar/NavbarCustom.jsx
+++ src/components/navbar/NavbarCustom.jsx
@@ -2,37 +2,42 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import '../navbar/NavbarCustom.module.css';
+import './NavbarCustom.module.css';
 import React from 'react';
 
 function NavbarCustom() {
   return (
-    <Navbar bg="dark" data-bs-theme="dark">
-    <Container>
-      <Navbar.Brand className='home' href="#home">
-      <img
-        src="/src/assets/images/Logo-Sabor-Argentino.png"
-        width="100"
-        height="100"
-        className="d-inline-block align-top"
-        alt="Logo Sabor Argentino"
-      />
-      </Navbar.Brand>
-      <Nav className="">
-        <Nav.Link className='navButton' href="#home">Home</Nav.Link>
-        <Nav.Link className='navButton' href="#features">Reservar</Nav.Link>
-        <Nav.Link className='navButton' href="#pricing">Galería</Nav.Link>
-        <NavDropdown title="Ingresá" id="basic-nav-dropdown">
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">
+        <img
+          src="/src/assets/images/Logo-Sabor-Argentino.png"
+          width="50"
+          height="50"
+          className="d-inline-block align-top"
+          alt="Logo Sabor Argentino"
+        />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="">
+          <Nav.Link className='navButton text-dark fw-bold' href="#home">Home</Nav.Link>
+        <Nav.Link className='navButton text-dark fw-bold' href="#features">Reservar</Nav.Link>
+        <Nav.Link className='navButton text-dark fw-bold' href="#pricing">Galería</Nav.Link>
+            <NavDropdown title="Ingresá" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Iniciar sesión</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
-                Registrarte
+                Registrarse
               </NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.3">Sobre nosotros</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.4">
+              Sobre nosotros
+              </NavDropdown.Item>
             </NavDropdown>
-      </Nav>
-    </Container>
-  </Navbar>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
 
