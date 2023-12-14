98b320dde590c0af5e19259e05b7e0ded0d738b4
nikoac <136534522+nikoac@users.noreply.github.com>
2023-12-14 01:46:48 -0300

    --

--- src/components/Navbar/NavbarCustom.jsx
+++ src/components/Navbar/NavbarCustom.jsx
@@ -1,26 +1,39 @@
 import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import '../navbar/NavbarCustom.style.css'
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import '../navbar/NavbarCustom.module.css';
+import React from 'react';
 
-const NavbarCustom = () => {
+function NavbarCustom() {
   return (
-    <>
-      <Navbar className="bg-body-tertiary">
-        <Container>
-          <Navbar.Brand href="#home">
-            <img
-              alt="Logo Sabor Argentino"
-              src="/src/assets/images/Logo-Sabor-Argentino.png"
-              width="60"
-              height="100"
-              className="d-inline-block align-top"
-            />{' '}
-            Sabor Argentino
-          </Navbar.Brand>
-        </Container>
-      </Navbar>
-    </>
-  )
+    <Navbar bg="dark" data-bs-theme="dark">
+    <Container>
+      <Navbar.Brand className='home' href="#home">
+      <img
+        src="/src/assets/images/Logo-Sabor-Argentino.png"
+        width="100"
+        height="100"
+        className="d-inline-block align-top"
+        alt="Logo Sabor Argentino"
+      />
+      </Navbar.Brand>
+      <Nav className="">
+        <Nav.Link className='navButton' href="#home">Home</Nav.Link>
+        <Nav.Link className='navButton' href="#features">Reservar</Nav.Link>
+        <Nav.Link className='navButton' href="#pricing">Galería</Nav.Link>
+        <NavDropdown title="Ingresá" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Iniciar sesión</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Registrarte
+              </NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.3">Sobre nosotros</NavDropdown.Item>
+            </NavDropdown>
+      </Nav>
+    </Container>
+  </Navbar>
+  );
 }
 
 export default NavbarCustom;
